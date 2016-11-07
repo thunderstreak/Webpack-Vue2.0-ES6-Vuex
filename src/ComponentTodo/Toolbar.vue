4b90f57a0113291f66c38b5a81f1b73a3bb14bfa
yb707893334 <821470904@qq.com>
2016-11-07 17:44:35 +0800

    17.440

---
+++ src/ComponentTodo/Toolbar.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+    <div id="toolbar">
+        <i @click="addNote" class="glyphicon glyphicon-plus"></i>
+        <i @click="toggleFavorite"
+          class="glyphicon glyphicon-star"
+          :class="{starred: activeNote.favorite}"></i>
+        <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>
+    </div>
+</template>
+
+<script>
+import {addNote,deleteNote,toggleFavorite} from '../statusBar/actions.js'
+
+export default {
+    vuex:{
+        getters:{
+            activeNote:state => state.activeNote
+        },
+        actions:{
+            addNote,
+            deleteNote,
+            toggleFavorite
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
